6f7d9e327e384e31ca3e606621665c3f89d4618e
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-10-10 00:04:38 -0300

    feat: Adiciona global styled

---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+    
+
+    div{
+        font-family: 'Roboto', sans-serif;
+    }
+
+    span{
+        font-family: 'Roboto', sans-serif;
+    }
+
+`
+
+export default GlobalStyle
