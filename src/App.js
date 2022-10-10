60f2006c57758da8e81e3de25af595a91e205391
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-10-10 14:10:11 -0300

    feat: Criando Rota privada para tela de inserir resultados

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignUp from "./pages/Signup/SignUp"
 import PainelControl from "./pages/TelaAdmin/PainelControl"
 import TelaInicial from "./pages/TelaInicialUser/TelaInicial"
 import TelaDay from "./pages/TelaMatchesByDay/TelaDay"
+import TelaInserirResultado from "./pages/TelaInserirResultado/TelaInserirResultado"
 
 import TokenContext from "./context/tokenContext"
 import UrlContext from "./context/urlContext"
@@ -35,7 +36,10 @@ function App() {
         <Route path="/" element={<SignIn/>}/>
         <Route path="/cadastro" element={<SignUp/>}/>
         <Route path="/home" element={<TelaInicial/>}/>
-        <Route path="/matchesByDay" element={<TelaDay/>}/>          
+        <Route path="/matchesByDay" element={<TelaDay/>}/>   
+
+        
+
 
         <Route 
         path = "/painel-control" 
@@ -47,7 +51,21 @@ function App() {
             <PainelControl/>
           </ProtectRouter>}
         />
-      </Routes>
+
+    <Route 
+      path="/insertResult" 
+      element={
+        <ProtectRouter 
+          path="/" 
+          permissions={permissions} 
+          permissionRequired={["view_admin_dashboard"]}>
+          
+          <TelaInserirResultado/>
+        </ProtectRouter>}
+    />
+
+
+      </Routes>      
 
       <GlobalStyle/>
     </BrowserRouter>
