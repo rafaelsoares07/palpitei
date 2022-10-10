838d7bced9df82246f243562e38ccdd2857d8087
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-10-10 00:07:14 -0300

    feat: Correções e mudanças no componente

--- src/pages/TelaInicialUser/Card.js
+++ src/pages/TelaInicialUser/Card.js
@@ -9,12 +9,14 @@ function CardFormat({matche,i}){
 
     const {token} = useContext(TokenContext)
     
-    const [s1, setS1] = useState("")  
-    const [s2, setS2] = useState("")
-    const [winner, setWinner] = useState("")
+    const [s1, setS1] = useState(undefined)  
+    const [s2, setS2] = useState(undefined)
+    const [winner, setWinner] = useState(null)
     const [isCorrect, setIsCorrect] = useState(undefined)
-    const [aux, setAux] = useState(false)
 
+    let hora = matche[i].dateAndHora
+    let formatHora = hora.substring(11,16)
+    
 
     const URL = "http://localhost:5001"
 
@@ -38,15 +40,17 @@ function CardFormat({matche,i}){
     }
     
    
-    if(winner!="" && s1!=undefined && s2!=undefined && aux==true){
+    if(winner!=null && s1!=undefined && s2!=undefined ){
         
         if(winner==="Empate"){
-            const promisse = axios.post(`${URL}/bets`,betWithWinner,config)
+            
+            const promisse = axios.post(`${URL}/bets`,betEmpate,config)
             promisse.then(el=>console.log(el))
             promisse.catch(err=>console.log(err))
         }
         else if(winner!=="Empate"){
-            const promisse = axios.post(`${URL}/bets`,betEmpate,config)
+           
+            const promisse = axios.post(`${URL}/bets`,betWithWinner,config)
             promisse.then(el=>console.log(el))
             promisse.catch(err=>console.log(err))
         }
@@ -56,51 +60,61 @@ function CardFormat({matche,i}){
  
     useEffect(()=>{
         
-        if(s1>s2 && s1!="" && s2!=""){
+        if(s1>s2 && s1!=undefined && s2!=undefined){
            
             setWinner(matche[i].Time1.id)
             
         }
-        else if(s1<s2 && s1!="" && s2!=""){
+        else if(s1<s2 && s1!=undefined && s2!=undefined){
             
             setWinner(matche[i].Time2.id)
                     
         }
-        else if(s1!="" && s2==""){
-            setWinner("")
+        else if(s1!=undefined && s2==undefined){
+            setWinner(null)
         }
-        else if(s1=="" && s2!=""){
-            setWinner("")
+        else if(s1==undefined && s2!=undefined){
+            setWinner(null)
         }
-        else if(s1=="" && s2==""){
-            setWinner("")
+        else if(s1==undefined && s2==undefined){
+            setWinner(null)
         }
-        else if (s1===s2){
+        else if (s1===s2 && s1!=undefined && s2!=undefined){
             setWinner("Empate")
         }
-
-        console.log('erro do userEffect 1')
 
     },[s1,s2])
 
     useEffect(()=>{
+        console.log("entrou aqui")
         const promisse = axios.get(`${URL}/bets/${matche[i].id}`,config)
 
+        
+
         promisse.then((response)=> {
+            console.log(response.data)
             setS1(response.data.gameScoreTimeOne)
             setS2(response.data.gameScoreTimeTwo)
             setIsCorrect(response.data.correct) 
         })
-        console.log('erro do userEffect 2')
         promisse.catch(err=>console.log(err))
 
-        setAux(true) 
+        
     },[])
 
 
     return(
         <Container>
+
+            <Header>
+                <span className="day">{matche[i].day} </span>
+                <span className="stadium"> {matche[i].Stadium.name}</span>
+                <span className="time">{formatHora}</span>
+            </Header>
+
+            
             <AreaMatches correct={isCorrect} >
+            
                 <Team>
                     {matche[i].Time1.sigla}
                     <img src={require(`../../images/bandeiras/${matche[i].Time1.sigla}.png`)}/>
@@ -108,11 +122,14 @@ function CardFormat({matche,i}){
                 </Team>
                 <span>x</span>
                 <Team>
+                
                     <input value={s2} onChange={(el)=>setS2(el.target.value)} type="number"></input>
                     <img src={require(`../../images/bandeiras/${matche[i].Time2.sigla}.png`)}/>
                     {matche[i].Time2.sigla}
-                    
                 </Team>
+
+           
+               
                 
             </AreaMatches>
         </Container>
@@ -129,14 +146,35 @@ const Container = styled.div`
     display: flex;
     align-items:center;
     justify-content: center;
+    flex-direction: column;
+    margin: 10px;
     
+`
+const Header = styled.div`
+
+    
+    
+    display:flex;
+    
+    span{
+        font-size: 14px;
+        margin: 2px;
+        
+    }
+    span.day, span.time{
+        font-weight:500;
+        color:#4F4F4F;
+    }
+
+
 `
 
 const AreaMatches = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-
+    
+    
     input{
         border-radius: 6px;
         border: none;
